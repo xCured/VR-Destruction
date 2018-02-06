9977305ba86de63361b54ec384c3329dde3b1054
Taha <tzach011@gold.ac.uk>
2018-02-06 22:55:10 +0000

    Forgot to add Scenes and new scripts

---
+++ VR-Destruction/Assets/Scripts/Scoreteller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreteller : MonoBehaviour {
+
+    //public GameObject AfterEnd;
+    public static bool whatsyourScore = true;
+
+   // BreakableObject.movableobj
+    // Use this for initialization
+
+        //if its false it dont move.
+        //if its true it moves.
+    void Start () {
+       
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+        if (whatsyourScore == false)
+        {
+            Appear();
+        }
+
+		
+	}
+
+    void Appear() {
+      gameObject.SetActive(true);
+       
+
+    }
+
+}
