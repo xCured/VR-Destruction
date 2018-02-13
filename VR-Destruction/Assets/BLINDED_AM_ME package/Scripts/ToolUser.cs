c07636be68ecb71bb1950fef39701b79aa4e01ba
Christian <cmugw002@gold.ac.uk>
2018-02-13 11:25:52 +0000

    added in files for slicing. not yet complete. blade does not cut at the correct places

---
+++ VR-Destruction/Assets/BLINDED_AM_ME package/Scripts/ToolUser.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolUser : MonoBehaviour {
+
+	public Material capMaterial;
+    public float bladeLength = 30;
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	void Update(){
+
+		
+			RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, bladeLength))
+            {
+
+                GameObject victim = hit.collider.gameObject;
+
+				GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+				if(!pieces[1].GetComponent<Rigidbody>())
+					pieces[1].AddComponent<Rigidbody>();
+
+				Destroy(pieces[1], 1);
+			}
+
+		}
+	
+
+	void OnDrawGizmosSelected() {
+
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + transform.up * 0.5f, transform.position + transform.up * 0.5f + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + -transform.up * 0.5f, transform.position + -transform.up * 0.5f + transform.forward * 5.0f);
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
+		Gizmos.DrawLine(transform.position,  transform.position + -transform.up * 0.5f);
+
+	}
+
+}
---
+++ VR-Destruction/Assets/Scripts/cuttableObject.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class cuttableObject : MonoBehaviour
+{
+
+
+   // PlayerScore playerScore;
+   // public GameObject scoreKeeper;
+    // public GameObject areaOfEffect;
+    
+   
+   
+
+    Vector3 startRot;
+    Vector3 startPos;
+    Vector3 endRot;
+    Vector3 endPos;
+
+    
+
+    //  public float minForce;
+   // private static int TimeLeft;
+   // public static bool moveableobj = true;
+
+
+    private void Start()
+    {
+       
+    }
+    //basically timer is int when that raches 0 the boolean doesnt change to true and idk why
+    void Update()
+    {
+
+
+
+    }
+
+    private void OnEnter(Collider col)
+    {
+
+
+       /* if (col.gameObject.tag != "breakable" && col.gameObject.tag != "plane" && col.gameObject.tag != "Untagged")
+        {
+            // Debug.Log(col.gameObject.name + ": " + col.gameObject.tag);*/
+            //Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), col.collider);
+       
+
+        //ContactPoint contact = col.contact;
+
+        
+            if (col.gameObject.tag == "cuttingTool")
+            {
+
+
+                 startRot = col.gameObject.transform.rotation.eulerAngles;
+                startPos = col.gameObject.transform.position;
+
+             
+            }
+        }
+
+
+    
+    
+
+
+    private void OnTriggerExit(Collider col)
+    {
+
+        //if (col.gameObject.tag == "cuttingTool")
+       // {
+
+
+            
+            endRot = col.gameObject.transform.rotation.eulerAngles;
+            endPos = col.gameObject.transform.position;
+            // Vector3 cutRot = (startRot + endRot) / 2;
+            Vector3 cutPos = (startPos + endPos)/2;
+
+            // Get score keeper score
+           // playerScore = scoreKeeper.GetComponent<PlayerScore>();
+
+            //get pointsgiven by object
+
+
+
+            //add points given to score keeper
+
+
+
+
+
+            // Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), unity.GetComponent<Collider>());
+
+            // gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            GameObject blade = Instantiate(Resources.Load("blade"), transform.position,col.gameObject.transform.rotation) as GameObject;
+           // Destroy(blade);
+            //aoe.GetComponent<AreaOfEffect>().setSize(col.relativeVelocity.magnitude);
+            //Destroy(aoe);
+            // rigid.AddForce(col.impulse * (5) + col.impulse.normalized, ForceMode.Impulse);
+            //rigid.AddForce(col.impulse * 1, ForceMode.Impulse);
+
+
+       // }
+       
+    }
+}
