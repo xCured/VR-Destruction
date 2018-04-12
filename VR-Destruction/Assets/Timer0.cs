d0f056a2bfcd8edd1265f90eddddd4b713cbb9ef
Taha Zacholi <tzach011@gold.ac.uk>
2018-04-12 15:31:45 +0100

    Changes to menu

---
+++ VR-Destruction/Assets/Timer0.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer0 : MonoBehaviour {
+
+    
+    public GameObject Menu;
+    public int MenuShower;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Timer.timeLeft = MenuShower;
+
+        if(Timer.timeLeft == 0)
+        {
+            
+            Menu.SetActive(true);
+            // gameObject.SetActive(true);
+        }
+
+        
+    }
+}
