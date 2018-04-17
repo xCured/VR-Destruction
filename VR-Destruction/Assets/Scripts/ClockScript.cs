b7c7e1d3f4fcd97621f223ac328f696142679a79
Taha Zacholi <tzach011@gold.ac.uk>
2018-04-17 13:40:25 +0100

    Fixed Menu issue with overall score

---
+++ VR-Destruction/Assets/Scripts/ClockScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "hammer" || col.gameObject.tag == "pickaxe" || col.gameObject.tag == "Axe")
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
---
+++ VR-Destruction/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionStay(Collision col)
+    {
+
+
+        ContactPoint contact = col.contacts[0];
+
+        if (col.gameObject.tag != "breakable" && col.gameObject.tag != "plane" && col.gameObject.tag != "Untagged")
+        {
+            // Debug.Log(col.gameObject.name + ": " + col.gameObject.tag);
+            Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), col.collider);
+        }
+
+        if (col.gameObject.tag == "hammer" || col.gameObject.tag == "pickaxe" || col.gameObject.tag == "Axe")
+        {
+            if ((gameObject.GetComponent<Rigidbody>().isKinematic == true))
+            {
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                Application.Quit();
+            }
+        }
+    }
+}
